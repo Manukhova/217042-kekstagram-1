c6e9080359ffdc2978073c2085ad07ba95197c0a
Manukhova <olga.manukhova@gmail.com>
2018-02-10 13:39:05 +0300

    done

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const args = process.argv.slice(2)[0];
 
-switch(args) {
-  case '--version':
-    console.log('v0.0.1');
+switch (args) {
+  case `--version`:
+    console.log(`v0.0.1`);
     break;
-  case '--help':
+  case `--help`:
     console.log(`Доступные команды:
       --help    — печатает этот текст;
       --version — печатает версию приложения`);
     break;
-  case undefined:
+  case `undefined`:
     console.log(`Привет пользователь!
       Эта программа будет запускать сервер «Kekstagram».
       Автор: Кекс.`);
     break;
   default:
-    let defaultMsg = '';
+    let defaultMsg = ``;
     process.argv.slice(2).forEach((item) => {
-      defaultMsg = `${defaultMsg} ${item}`
+      defaultMsg = `${defaultMsg} ${item}`;
     });
     console.error(`
       Неизвестная команда ${defaultMsg}.
-      Чтобы прочитать правила использования приложения, наберите "--help"'`
+      Чтобы прочитать правила использования приложения, наберите "--help"`
     );
     process.exit(1);
 }
