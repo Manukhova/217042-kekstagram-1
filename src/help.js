3ad093b965fd07e2bf51c10618d22bbf42d506c0
Manukhova <olga.manukhova@gmail.com>
2018-02-12 20:23:49 +0300

    fixes added

--- src/help.js
+++ src/help.js
@@ -1,11 +1,18 @@
 const colors = require(`colors`);
+const version = require(`./version`);
+const author = require(`./author`);
+const license = require(`./license`);
+const description = require(`./description`);
 
 module.exports = {
   name: `help`,
   description: `Shows available commands`,
   execute() {
     console.log(`Доступные команды:
-      --${colors.gray(`help`)} — ${colors.green(`печатает этот текст`)};
-      --${colors.gray(`version`)} — ${colors.green(`печатает версию приложения`)}`);
+      --${colors.gray(this.name)} — ${colors.green(this.description)};
+      --${colors.gray(version.name)} — ${colors.green(version.description)};
+      --${colors.gray(license.name)} — ${colors.green(license.description)};
+      --${colors.gray(author.name)} — ${colors.green(author.description)};
+      --${colors.gray(description.name)} — ${colors.green(description.description)}`);
   }
 };
