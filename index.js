f22bf2e14203036140fa6352c55b1aa147473185
Manukhova <olga.manukhova@gmail.com>
2018-02-24 14:38:16 +0300

    review fixes added

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const author = require(`./src/author`);
 const license = require(`./src/license`);
 const description = require(`./src/description`);
 const byDefault = require(`./src/default`);
-const server = require(`./server`);
+const serverCommand = require(`./src/serverCommand`);
 
 const args = process.argv.slice(2);
-
-const PORT_ADRESS = 3000;
 
 let map = new Map();
 
@@ -19,12 +17,10 @@ map
     .set(`--${author.name}`, author)
     .set(`--${license.name}`, license)
     .set(`--${description.name}`, description)
+    .set(`--${serverCommand.name}`, serverCommand)
     .set(void 0, generate);
 
-if (args[0] === `--server`) {
-  const port = args[1] ? args[1] : PORT_ADRESS;
-  server.run(port);
-} else if (map.has(args[0])) {
+if (map.has(args[0])) {
   map.get(args[0]).execute();
 } else {
   byDefault.execute();
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const http = require(`http`);
 const fs = require(`fs`);
+const url = require(`url`);
 const path = require(`path`);
 const {promisify} = require(`util`);
 
-const stat = promisify(fs.stat);
 const readfile = promisify(fs.readFile);
-
-const hostname = `127.0.0.1`;
 
 const types = {
   '.css': `text/css`,
@@ -21,28 +19,23 @@ const readFile = async (absolutePath, res) => {
   const data = await readfile(absolutePath);
 
   const headers = {};
-
-  const splitedPathArr = absolutePath.split(`/`);
-  const contentType = types[path.extname(splitedPathArr[splitedPathArr.length - 1])];
+  const contentType = types[path.extname(absolutePath)];
 
   if (contentType) {
-    headers[`Content-Type`] = contentType;
-    res.writeHead(200, headers);
+    res.setHeader(`Content-Type`, contentType);
+    res.writeHead(200, `OK`);
     res.end(data);
   }
 };
 
 const server = http.createServer((req, res) => {
-  let absolutePath;
+  let absolutePath = __dirname + `/static` + url.parse(req.url).pathname;
 
   (async () => {
     try {
       if (req.url === `/`) {
         absolutePath = __dirname + `/static/index.html`;
-      } else {
-        absolutePath = __dirname + `/static` + req.url;
       }
-      await stat(absolutePath);
       await readFile(absolutePath, res);
     } catch (e) {
       console.log(e);
@@ -57,13 +50,4 @@ const server = http.createServer((req, res) => {
   });
 });
 
-module.exports = {
-  run(port) {
-    server.listen(port, hostname, (e) => {
-      if (e) {
-        return console.error(e);
-      }
-      return console.log(`Server runs at ${hostname}:${port}`);
-    });
-  }
-};
+module.exports = server;
